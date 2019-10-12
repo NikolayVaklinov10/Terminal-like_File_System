1968503bd94ef21e8c4213a983cc86db33f63f24
Nick <nikolayvaklinov@yahoo.com>
2019-10-12 13:17:33 +0100

    more thins

--- src/com/rtjvm/scala/oop/commands/UnknownCommand.scala
+++ src/com/rtjvm/scala/oop/commands/UnknownCommand.scala
@@ -2,9 +2,7 @@ package com.rtjvm.scala.oop.commands
 
 import com.rtjvm.scala.oop.filesystem.State
 
-/**
-  * Created by Daniel on 28-Oct-17.
-  */
+
 class UnknownCommand extends Command {
 
   override def apply(state: State): State =
