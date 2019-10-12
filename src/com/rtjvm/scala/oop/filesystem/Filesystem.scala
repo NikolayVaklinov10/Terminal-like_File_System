8bd2a952a29024ce154d11672260e885c93d7e92
Nick <nikolayvaklinov@yahoo.com>
2019-10-12 13:19:40 +0100

    the entry point

--- src/com/rtjvm/scala/oop/filesystem/Filesystem.scala
+++ src/com/rtjvm/scala/oop/filesystem/Filesystem.scala
@@ -1,8 +1,25 @@
 package com.rtjvm.scala.oop.filesystem
 
-object Filesystem extends App{
+import java.util.Scanner
 
+import com.rtjvm.scala.oop.commands.Command
+import com.rtjvm.scala.oop.files.Directory
 
+object Filesystem extends App {
 
+  val root = Directory.ROOT
 
+  // [1, 2, 3, 4]
+  /*
+   0 (op) 1 => 1
+   1 (op) 2 => 3
+   3 (op) 3 => 6
+   6 (op) 4 => your last value, 10
+
+   List(1,2,3,4).foldLeft(0)((x, y) => x + y)
+    */
+  io.Source.stdin.getLines().foldLeft(State(root, root))((currentState, newLine) => {
+    currentState.show
+    Command.from(newLine).apply(currentState)
+  })
 }
